7057fe4830443a113d19421a9bdf6f1b5a74b00f
chudistar01 <christianigwebuike31@gmail.com>
2023-12-03 00:44:25 -0800

    typedef and struct projects

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,10 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
 
-		printf("Age: %f\n", d->age);
+		printf("Age: %6f\n", d->age);
 
 		if (d->owner != NULL)
 		{
@@ -26,7 +26,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 	}
 }

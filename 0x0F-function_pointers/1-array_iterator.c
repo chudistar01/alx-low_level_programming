60564cabbaf845dd9aa603703cbde8ee0109000a
chudistar01 <christianigwebuike31@gmail.com>
2023-12-09 23:46:13 -0800

    function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
-	if (array != NULL || action != NULL)
+	if (array &&  action)
 	{
 		for (i = 0; i < size; i++)
 		{

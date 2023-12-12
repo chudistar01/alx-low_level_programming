ed57b1d044d3d92c7dac91191de8e90774d5d195
chudistar01 <christianigwebuike31@gmail.com>
2023-12-11 23:22:57 -0800

    singly list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
+	const listint_t *tp;
+	tp = h
 
-	while (h)
+	while (tp)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n",tp->n);
 		num++;
-		h = h->next;
+		tp = tp->next;
 	}
 	return (num);
 }

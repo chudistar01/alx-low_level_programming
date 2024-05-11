b21734f93f38b903ca88398ac876e34aeca1efff
chudistar01 <christianigwebuike31@gmail.com>
2024-05-11 04:38:44 -0700

    function pointers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: number of arguements to be provided
+ *
+ * Return: resulting sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list list;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif

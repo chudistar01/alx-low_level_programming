15a27130230207b4887d06e97de16e6e63e829f7
chudistar01 <christianigwebuike31@gmail.com>
2024-05-21 19:25:10 -0700

    searc algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,35 +1,41 @@
 #include "search_algos.h"
 /**
- * binary_search - searches for a value in a sorted
- * @array: array to be searched
- * @size: size of the array
+ * binary_search - binary searched
+ * @array: pointer to an array
+ * @size: size of array
  * @value: value to be searched
- * Return: index of value
+ * Return: index of the serached value
  */
 int binary_search(int *array, size_t size, int value)
 {
-  size_t i, left, right;
-  if (array == NULL)
-	  return (-1);
-  left = 0;
-  right = size - 1;
-  while (right >= left)
-  {
-	  printf("Searching in array: ");
-	  i = left;
-	  while (i < right)
-	  {
-		  printf("%d, ", array[i]);
-		  i++;
-	  }
-	  printf("%d\n", array[i]);
-	  i = left + (right - left) / 2;
-	  if (array[i] == value)
-		  return (i);
-	  if (array[i] > value)
-		  right = i - 1;
-	  else
-		  left = i + 1;
-  }
-  return (-1);
+	size_t i, left, right;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	left = 0;
+	right = size - 1;
+
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		i = left;
+		while (i < right)
+		{
+			printf("%d, ", array[i]);
+			i++;
+		}
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+	return (-1);
 }

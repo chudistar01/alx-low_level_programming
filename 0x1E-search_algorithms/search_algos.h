90e7233c02d28cf0b3fe86d21cbebaf9a0befd9e
chudistar01 <christianigwebuike31@gmail.com>
2024-05-22 08:18:07 -0700

    search algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif

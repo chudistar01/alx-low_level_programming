f82174e51c2eed5a19e0666771dd045c40c86848
chudistar01 <christianigwebuike31@gmail.com>
2024-05-24 00:38:58 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+Wget -P /tmp/ https://raw.github.com/chudistar01/alx-low_level_programming/master/0x18-dynamic_libraries/test.so
+export LD_PRELOAD=/tmp/test.so
---
+++ 0x18-dynamic_libraries/test.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * printf - prints characters
+ * @format: pointer to character to print
+ * Return: integer
+ */
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 -9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
+}

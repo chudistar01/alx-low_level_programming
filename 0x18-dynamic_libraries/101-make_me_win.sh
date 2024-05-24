02b3d0b67551bcca7f6837ed4797b4625b899bfb
chudistar01 <christianigwebuike31@gmail.com>
2024-05-24 00:44:18 -0700

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 Wget -P /tmp/ https://raw.github.com/chudistar01/alx-low_level_programming/master/0x18-dynamic_libraries/test.so
-export LD_PRELOAD=/$PWD/libtest.so
+export LD_PRELOAD=/$PWD/libtest.so ./gm 9 8 10 24 75 9

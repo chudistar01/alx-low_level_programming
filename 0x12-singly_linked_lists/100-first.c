28889d23cbbf9a7fb1a47354023fa5559ae7b6eb
chudistar01 <christianigwebuike31@gmail.com>
2024-06-23 05:31:46 -0700

    singly_list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@ void before_main()__attribute__((constructor));
  * before_main - prints sentence before main
  *
  */
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
